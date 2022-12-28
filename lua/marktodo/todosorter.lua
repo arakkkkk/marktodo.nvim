1eb01b733361e417154353ebd5ed80d04916eec2
kinoshitalab <kouiti@arakkk.local>
2022-12-28 17:49:39 +0900

    update

--- lua/marktodo/todosorter.lua
+++ lua/marktodo/todosorter.lua
@@ -2,7 +2,7 @@ local marktodo = require("marktodo")
 local M = {}
 
 function M.sort(parsers)
-	local function isHead(or_head_parser, compare_parser, target)
+	local function swhich(or_head_parser, compare_parser, target)
 		if type(or_head_parser[target]) ~= "string" then
 			return false
 		end
@@ -13,20 +13,15 @@ function M.sort(parsers)
 			return true
 		end
 		if target == "priority" then
-			print(
-				or_head_parser[target] < compare_parser[target],
-				":",
-				compare_parser[target] .. ", " .. or_head_parser[target]
-			)
 			return or_head_parser[target] < compare_parser[target]
 		end
 		assert(target)
 	end
 
 	for _, sort_target in pairs(marktodo.ops.sort) do
-		for i = 1, #parsers do
-			for j = i, #parsers - 1 do
-				if isHead(parsers[j + 1], parsers[j], sort_target) then
+		for i = #parsers, 1, -1 do
+			for j = 1, i - 1 do
+				if swhich(parsers[j + 1], parsers[j], sort_target) then
 					local tail = parsers[j]
 					parsers[j] = parsers[j + 1]
 					parsers[j + 1] = tail
--- test/mock/marktodos.md
+++ test/mock/marktodos.md
@@ -1,10 +1,10 @@
 -- [Corrent](2022-12-28_corrent.md)
 - [ ] (A) 2022-12-27 2022-12-26 sample todo @context +Project due:2022-12-27 tag:value
-- [ ] (A) 2022-12-27 2022-12-26 sample todo @context +Project due:2022-12-27 tag:value
+- [X]  2022-12-27 2022-12-26 sample todo @context +Project due:2022-12-27 tag:value
 - [ ] () 2022-12-27 2022-12-26 sample todo @context +Project due:2022-12-27 tag:value
 - [ ] ( ) 2022-12-27 2022-12-26 sample todo @context +Project due:2022-12-27 tag:value
 - [ ] (D) 2022-12-26 sample todo @context +Project due:2022-12-27 tag:value
-- [ ] (A) sample todo @context +Project due:2022-12-27 tag:value
+- [ ]  sample todo @context +Project due:2022-12-27 tag:value
 - [ ] (A) 2022-12-27 2022-12-26 sample todo
 - [ ] (A) 2022-12-27 2022-12-26 @context +Project due:2022-12-27 tag:value
 - [ ] (B) 2022-12-26 sample todo @context +Project due:2022-12-27 tag:value
