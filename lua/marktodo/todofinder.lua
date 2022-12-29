c5cbc498d01557344830d15efdae84922c4cba15
kinoshitalab <kouiti@arakkk.local>
2022-12-29 16:36:55 +0900

    update

--- lua/marktodo/todofinder.lua
+++ lua/marktodo/todofinder.lua
@@ -7,10 +7,10 @@ function M.find(root_path, filter)
 	filter.priotiry = filter.priotiry or "A-Z"
 	filter.due = filter.due or "A-Z"
 
+	print(root_path)
+	print(root_path)
+	print(root_path)
 	local todo_lines = grep(root_path, marktodo.marktodo_pattern, "-tmd", marktodo.ops.exclude_ops)
-	print(#todo_lines)
-	print(#todo_lines)
-	print(#todo_lines)
 	return todo_lines
 end
 
