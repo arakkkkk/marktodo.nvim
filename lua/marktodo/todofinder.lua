12cd16f6dbf415152866bc65a09641125ce755ca
kinoshitalab <kouiti@arakkk.local>
2022-12-29 16:31:26 +0900

    update

--- lua/marktodo/todofinder.lua
+++ lua/marktodo/todofinder.lua
@@ -8,8 +8,6 @@ function M.find(root_path, filter)
 	filter.due = filter.due or "A-Z"
 
 	local todo_lines = grep(root_path, marktodo.marktodo_pattern, "-tmd", marktodo.ops.exclude_ops)
-	print(#todo_lines)
-	print(#todo_lines)
 	return todo_lines
 end
 
